1315eb7f42468fc7a4b252c383c94dda818d701b
jaeseongDev <qkrwotjd1445@naver.com>
2025-05-05 20:52:25 +0900

    refactor: 불필요한 코드 삭제

--- src/main/java/com/example/coupangapiserver/product/domain/Product.java
+++ src/main/java/com/example/coupangapiserver/product/domain/Product.java
@@ -2,8 +2,6 @@ package com.example.coupangapiserver.product.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
--- src/main/java/com/example/coupangapiserver/product/domain/ProductCategory.java
+++
@@ -1,9 +0,0 @@
-package com.example.coupangapiserver.product.domain;
-
-public enum ProductCategory {
-  ELECTRONICS,
-  CLOTHING,
-  HOME_APPLIANCES,
-  BOOKS,
-  BEAUTY;
-}
