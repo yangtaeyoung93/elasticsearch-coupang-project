9c1b83bdbbc6ac07356f3e3e11c49b8b675fd12b
jaeseongDev <qkrwotjd1445@naver.com>
2025-05-08 20:29:20 +0900

    feat: Delete API 파라미터 수정

--- src/main/java/com/example/coupangapiserver/product/ProductController.java
+++ src/main/java/com/example/coupangapiserver/product/ProductController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,8 +36,8 @@ public class ProductController {
         return ResponseEntity.ok(product);
     }
 
-    @DeleteMapping()
-    public ResponseEntity<Void> deleteProduct(@RequestParam Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
         return ResponseEntity.noContent().build();
     }
