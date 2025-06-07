420287fdb4cd13dfd6aecfef97f9ea78d77a5b36
yangtaeyoung93 <ngj100@naver.com>
2025-06-07 17:34:35 +0900

    Add Mysql, Elasticsearch 동기화

--- src/main/java/com/example/coupangapiserver/product/ProductService.java
+++ src/main/java/com/example/coupangapiserver/product/ProductService.java
@@ -1,6 +1,7 @@
 package com.example.coupangapiserver.product;
 
 import com.example.coupangapiserver.product.domain.Product;
+import com.example.coupangapiserver.product.domain.ProductDocument;
 import com.example.coupangapiserver.product.dto.CreateProductRequestDto;
 import java.util.List;
 import org.springframework.data.domain.PageRequest;
@@ -11,9 +12,12 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
   private final ProductRepository productRepository;
+  private final ProductDocumentRepository productDocumentRepository;
 
-  public ProductService(ProductRepository productRepository) {
+
+  public ProductService(ProductRepository productRepository, ProductDocumentRepository productDocumentRepository) {
     this.productRepository = productRepository;
+    this.productDocumentRepository = productDocumentRepository;
   }
 
   public List<Product> getProducts(int page, int size) {
@@ -22,6 +26,7 @@ public class ProductService {
   }
 
   public Product createProduct(CreateProductRequestDto createProductRequestDto) {
+    // MySQL 등록
     Product product = new Product(
         createProductRequestDto.getName(),
         createProductRequestDto.getDescription(),
@@ -29,10 +34,27 @@ public class ProductService {
         createProductRequestDto.getRating(),
         createProductRequestDto.getCategory()
     );
-    return productRepository.save(product);
+    Product savedProduct = productRepository.save(product);
+
+    //Elasticsearch 등록
+    ProductDocument productDocument = new ProductDocument(
+            String.valueOf(savedProduct.getId()),
+            savedProduct.getName(),
+            savedProduct.getDescription(),
+            savedProduct.getPrice(),
+            savedProduct.getRating(),
+            savedProduct.getCategory()
+    );
+
+    productDocumentRepository.save(productDocument);
+
+    return savedProduct;
   }
 
+
+
   public void deleteProduct(Long id) {
-    productRepository.deleteById(id);
+    productRepository.deleteById(id); //MySQL 삭제
+    productDocumentRepository.deleteById(String.valueOf(id)); //Elasticsearch 삭제
   }
 }
